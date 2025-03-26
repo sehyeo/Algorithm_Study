6add6ed9754164c9c90f83af210e25de86645058
김승현 <145731052+sehyeo@users.noreply.github.com>
2025-03-26 21:17:04 +0900

    [Bronze III] Title: 최소, 최대, Time: 1528 ms, Memory: 310600 KB -BaekjoonHub

---
+++ 백준/Bronze/10818. 최소， 최대/최소， 최대.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        int N = sc.nextInt();
+        int min = Integer.MAX_VALUE;
+        int max = Integer.MIN_VALUE;
+        
+        for(int i=0;i<N;i++){
+            int num = sc.nextInt();
+            if (num < min) min = num;
+            if (num > max) max = num;
+        }
+        System.out.println(min + " " + max);
+    }
+}
