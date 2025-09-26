c041a02474cd6ba0c5501fa21d40492c79d23be2
김승현 <145731052+sehyeo@users.noreply.github.com>
2025-09-26 14:07:35 +0900

    [level 0] Title: 코드 처리하기, Time: 372.25 ms, Memory: 374 MB -BaekjoonHub

---
+++ 프로그래머스/0/181932. 코드 처리하기/코드 처리하기.java
@@ -0,0 +1,32 @@
+class Solution {
+    public String solution(String code) {
+        String ret = "";
+        int mode = 0;
+        
+        for (int idx = 0; idx < code.length(); idx++) {
+            if (mode == 0) {
+                if (code.charAt(idx) == '1') {
+                    mode = 1;
+                } else {
+                    if (idx % 2 == 0) {
+                        ret += code.charAt(idx);
+                    }
+                }
+            } else if (mode == 1) {
+                if (code.charAt(idx) == '1') {
+                    mode = 0;
+                } else {
+                    if (idx % 2 == 1) {
+                        ret += code.charAt(idx);
+                    }
+                }
+            }
+        }
+        
+        if (ret.equals("")) {
+            ret = "EMPTY";
+        }
+        
+        return ret;
+    }
+}
